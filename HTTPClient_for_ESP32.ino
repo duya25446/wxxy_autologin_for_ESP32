d92aff3f2b64448f42ed5d2bb69d28ae124a4483
duya25446 <i145463912@yahoo.com>
2022-11-14 12:32:23 +0800

    updata

---
+++ HTTPClient_for_ESP32.ino
@@ -0,0 +1,132 @@
+/**
+ * BasicHTTPClient.ino
+ *
+ *  Created on: 24.05.2015
+ *
+ */
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+WiFiMulti wifiMulti;
+
+/*
+const char* ca = \ 
+"-----BEGIN CERTIFICATE-----\n" \  
+"MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \  
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \  
+"DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \  
+"SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \  
+"GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \  
+"AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \  
+"q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \  
+"SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \  
+"Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \  
+"a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \  
+"/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \  
+"AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \  
+"CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \  
+"bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \  
+"c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \  
+"VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \  
+"ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \  
+"MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \  
+"Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \  
+"AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \  
+"uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \  
+"wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \  
+"X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \  
+"PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \  
+"KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \  
+"-----END CERTIFICATE-----\n";
+*/
+String GetIp()
+{
+
+    String ip;
+    HTTPClient http;
+    USE_SERIAL.print("[HTTP] GET IP begin...\n");
+    http.begin("http://10.1.99.100/drcom/chkstatus?callback=dr1002&jsVersion=4.X&v=4299&lang=zh");
+    int httpCode = http.GET();
+    if(httpCode > 0)
+    {
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+      if(httpCode == HTTP_CODE_OK)
+      {
+        String resp = http.getString();
+        int i = resp.indexOf("\"v46ip\":\"")+9;
+        int n = resp.indexOf("\",\"myv6ip\"");//计算
+        ip = resp.substring(i,n);
+        USE_SERIAL.println(ip);
+        //复制到
+      }
+    }
+    else
+    {
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());    
+    }
+    http.end();
+    return ip;
+}
+
+void login(String ip,String user_account,String yys,String password)
+{
+  String auth = user_account + "@" + yys;
+  String url = "http://10.1.99.100:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=,0," + auth + "&user_password=" + password + "&wlan_user_ip=" + ip + "&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=1&lang=zh-cn&v=10114&lang=zh";
+  USE_SERIAL.println(url);
+  HTTPClient http;
+  USE_SERIAL.print("[HTTP] Login begin...\n");
+  http.begin(url);
+  int httpCode = http.GET();
+  if(httpCode > 0)
+    {
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+      if(httpCode == HTTP_CODE_OK)
+      {
+        String resp = http.getString();
+        USE_SERIAL.println(resp);
+        //复制到
+      }
+    }
+    else
+    {
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());    
+    }
+    http.end();
+
+}
+
+void setup()
+{
+
+  USE_SERIAL.begin(115200);
+
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  for(uint8_t t = 4; t > 0; t--) 
+  {
+    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+  wifiMulti.addAP("duya25446", "Duya25446");
+  while(wifiMulti.run() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  login(GetIp(),"20202378024","telecom","283835");
+}
+
+void loop() 
+{
+
+}
